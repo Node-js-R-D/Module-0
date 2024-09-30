a526ab39c36f91a4750c9d7ca49594e041991b1c
Roman <vrbox@i.ua>
2024-09-30 13:21:46 +0300

    Done

--- task1.js
+++ task1.js
@@ -1,1 +1,12 @@
 
+function spesialNumbersOfRange(upRange) {
+    const answerMas = [];
+    for (let index = 1; index <= upRange; index++) {
+        if (index % 3 === 0 || index % 5 === 0 || index % 7 === 0) {
+            answerMas.push(index);
+        }
+    }
+    return answerMas;
+}
+
+console.log(spesialNumbersOfRange(20));
